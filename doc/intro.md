2e577f0b4e41afc6f5cbfd235324009a2c35d3ef
Erikadionn <erikadionn@gmail.com>
2018-08-17 20:40:06 -0400

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dinnertime
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dinnertime/core_test.clj
@@ -0,0 +1,7 @@
+(ns dinnertime.core-test
+  (:require [clojure.test :refer :all]
+            [dinnertime.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
